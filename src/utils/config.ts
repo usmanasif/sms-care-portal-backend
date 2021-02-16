c474bdc663383f9c3acd94383644c182cbd9510b
Ryan Weald <ryan@weald.com>
2021-02-16 12:50:15 -0800

    Rip out dotenv dependency which was only used in dev.
    dotenv is not really the right solution to our configuration needs since
    Aptible has it's own system for sharing env variables with the building
    and running containers. The project already only used it for
    development, so no reason the have it as a dependency when people can
    just use their own local solution for managing env variables per
    directory. I highly recommend direnv ;)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,20 +1,3 @@
-import { config as configDotenv } from 'dotenv';
-import { resolve } from 'path';
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    console.log("Environment is 'development'");
-    configDotenv({
-      path: resolve(__dirname, '../../.env.development'),
-    });
-    break;
-  case 'production':
-    console.log("Environment is 'production'");
-    break;
-  default:
-    throw new Error(`'NODE_ENV' ${process.env.NODE_ENV} is not handled!`);
-}
-
 const DATABASE_URI = process.env.DATABASE_URI || '';
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
