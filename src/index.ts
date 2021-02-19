f9fd22462ff6936e2a78e23bea096246b88f0878
Ryan Weald <ryan@weald.com>
2021-02-18 16:35:47 -0800

    Node should expose port 3000 by default.
    This is a standard port for all our docker containers to expose, which
    makes configuring docker or aptible networking much easier

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ connectToDatabase((err) => {
   if (err) console.log(err);
 });
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
