3c42c3df92545c063a1c1f04c24ab567693e43a7
Ryan Weald <ryan@weald.com>
2021-02-22 22:11:18 -0800

    Add first test of some basic Twilio config logic

--- test/utils/config.test.ts
+++ test/utils/config.test.ts
@@ -1,6 +1,13 @@
+import {parseTwilioFromNumber} from '../../src/utils/config';
 
 describe('parseTwilioFromNumber', () => {
   it('throws an error if no process values is set', () => {
+    expect(() => {
+      parseTwilioFromNumber(undefined);
+    }).toThrow();
+  });
 
+  it('strips unwanted characters from the number', () => {
+    expect(parseTwilioFromNumber('408-460-9195')).toEqual('4084609195');
   });
 });
